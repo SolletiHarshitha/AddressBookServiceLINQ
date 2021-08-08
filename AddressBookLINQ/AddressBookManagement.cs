1389132fe718548913a4ac7b6b01bc1ef30a041a
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-08 16:55:58 +0530

    [Harshitha] Adding columns to the DataTable

--- AddressBookLINQ/AddressBookManagement.cs
+++ AddressBookLINQ/AddressBookManagement.cs
@@ -7,12 +7,22 @@ using System.Threading.Tasks;
 
 namespace AddressBookLINQ
 {
-    class AddressBookManagement
+    public class AddressBookManagement
     {
         public AddressBookManagement()
         {
             //Creating Data Table
             DataTable dataTable = new DataTable();
+
+            //Adding Columns to the Data Table
+            dataTable.Columns.Add("FirstName");
+            dataTable.Columns.Add("LastName");
+            dataTable.Columns.Add("Address");
+            dataTable.Columns.Add("City");
+            dataTable.Columns.Add("State");
+            dataTable.Columns.Add("Zip");
+            dataTable.Columns.Add("PhoneNumber");
+            dataTable.Columns.Add("Email");
         }
     }
 }
