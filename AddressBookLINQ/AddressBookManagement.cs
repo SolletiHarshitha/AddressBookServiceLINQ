5e76394d2da32ffd0bf90eb9bd3081a4d56857ac
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-08 16:46:19 +0530

    [Harshitha] Creating DataTable

---
+++ AddressBookLINQ/AddressBookManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    class AddressBookManagement
+    {
+        public AddressBookManagement()
+        {
+            //Creating Data Table
+            DataTable dataTable = new DataTable();
+        }
+    }
+}
